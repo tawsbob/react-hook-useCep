f3eb46cf966b0b4b099beecc411d4ae3485a1890
tawsbob <dellean.santos@gmail.com>
2021-05-02 17:31:06 -0300

    fix build bug

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ var path = require('path');
 module.exports = {
   entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, 'build'),
+    path: path.resolve(__dirname, 'dist'),
     filename: 'index.js',
     library: 'index',
     globalObject: "this",
